f460d28c5efc04b8109a100dce6ee9f91eb382db
sobotab <sobotab@udel.edu>
2021-09-28 20:37:58 -0400

    adding email form

---
+++ src/emailForm.js
@@ -0,0 +1,10 @@
+let submitButton=document.getElementById("submit_button")
+let fname=document.getElementById("user_fname")
+let lname=document.getElementById("user_lname")
+let userEmail=document.getElementById("user_email")
+let phoneNumber=document.getElementById("user_phone")
+let userMessage=document.getElementById("user_message")
+
+submitButton.onclick(function() {
+    
+})
