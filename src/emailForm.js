5a20b29877495a1adf2418d7c08c8b3a6f2c585b
sobotab <sobotab@udel.edu>
2021-09-28 21:41:34 -0400

    email form sends me an email

--- src/emailForm.js
+++ src/emailForm.js
@@ -1,10 +1,15 @@
-let submitButton=document.getElementById("submit_button")
-let fname=document.getElementById("user_fname")
-let lname=document.getElementById("user_lname")
-let userEmail=document.getElementById("user_email")
-let phoneNumber=document.getElementById("user_phone")
-let userMessage=document.getElementById("user_message")
+let User_ID="user_enkWKyFhTz4LYcC40AIa0"
+let Access_Token="62f0bc3d7f354a23b46900e3a3e6b0ca"
 
-submitButton.onclick(function() {
-    
-})
+window.onload = function() {
+    document.getElementById("contact_form").addEventListener('submit', function(event) {
+        event.preventDefault();
+        emailjs.sendForm('service_sxczv1e', 'template_8le72v7', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+            console.log('FAILED...',error);
+            });
+    });
+}
+
