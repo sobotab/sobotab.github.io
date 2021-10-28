7110b197a37026ba83df411a30aadeced9339488
sobotab <sobotab@udel.edu>
2021-10-28 09:10:17 -0400

    added about me section

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 setTimeout(() => {$(document).ready(function() {
-    var top1 = $('#home').offset().top;
-    console.log(top1);
-    var top2 = $('#about-me').offset().top;
-    console.log(top2);
-    var top3 = $('#projects').offset().top;
-    var top4 = $('#contact').offset().top;
+    var top1 = $('#home').offset().top-10;
+    var top2 = $('#about-me').offset().top-10;
+    var top3 = $('#projects').offset().top-10;
+    var top4 = $('#contact').offset().top-10;
 
     var selectColor="lightgreen";
     var color = "white";
 
     $(document).scroll(function () {
         var scrollPos = $(document).scrollTop();
-        console.log(scrollPos);
         if (scrollPos >= top1 && scrollPos < top2) {
             $('#nav-home').css('background-color', selectColor);
             $('#nav-about-me').css('background-color', color);
